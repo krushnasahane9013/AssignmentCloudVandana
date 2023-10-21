2a76f6a89992ae17852b579e91d0aa65cdd96c80
Krushna Sahane <krushnasahane9013@gmail.com>
2023-10-21 13:08:16 +0530

    Sorting the array manually

--- src/JavaScript/SortingofArray.js
+++ src/JavaScript/SortingofArray.js
@@ -1,10 +1,19 @@
+//SOrt the array in descending order
 
 const arr = [5, 2, 9, 1, 5, 6];
+console.log("Original array: ", arr);
+let sortedArray = []
+for(let i=0; i<arr.length-1; i++){
+	for(let j = i+1 ; j< arr.length;j++)
+        {
+            if(arr[i] <  arr[j])
+            {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+}
 
-arr.sort(function(a, b) 
-/* return statement to sort array in decending order */
-{
-  return b - a;
-});
 
-console.log(arr);
+console.log("Sorted array: ", arr);
